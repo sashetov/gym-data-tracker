c661358832071e1f1752a866382ed4ca91a23da7
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2024-02-18 22:33:24 -0800

    added dashboard to grafana, added a custom metric to the docker image

---
+++ README.md
@@ -0,0 +1,4 @@
+Grafana dashboard for app container:
+    http://ad7cafbf60e36469cb437df78d77dae7-1891262572.us-west-2.elb.amazonaws.com/d/d1e229ef-d637-47fb-be4b-13e52eea69b8/app-container-metrics?from=1708297127081&to=1708319884224&orgId=1&theme=dark
+App Itself:
+    http://a9ab980022dea4fb1ab968bcce5b582b-124879317.us-west-2.elb.amazonaws.com/
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 import hashlib
 import os
-
+from prometheus_client import Counter
+cpu_views = Counter('cpu_gets', '/cpu GETs')
+mem_views = Counter('mem_gets', '/memory GETs')
 app = Flask(__name__)
 
 # Endpoint to generate CPU load by calculating hash
@@ -9,12 +11,14 @@ app = Flask(__name__)
 def cpu_load():
     for _ in range(1000000):
         hashlib.sha256(os.urandom(1024)).hexdigest()
+    cpu_views.inc()
     return 'CPU load generated'
 
 # Endpoint to generate Memory load by allocating memory
 @app.route('/memory')
 def memory_load():
     some_large_var = ' ' * 10**7  # Allocate a string with 10 million spaces
+    mem_views.inc()
     return 'Memory load generated'
 
 if __name__ == '__main__':
