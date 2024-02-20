d46a9b692815c1bcf9dbf396da9b6d60928693c7
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2024-02-19 21:32:10 -0800

    added mysql db to k8s

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from flask import Flask
-import hashlib
-import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@mysql-service/db_name'
+db = SQLAlchemy(app)
 
-@app.route('/cpu')
-def cpu_load():
-    for _ in range(1000000):
-        hashlib.sha256(os.urandom(1024)).hexdigest()
-    return 'CPU load generated'
-
-@app.route('/memory')
-def memory_load():
-    some_large_var = ' ' * 10**7  # Allocate a string with 10 million spaces
-    return 'Memory load generated'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- install.sh
+++ install.sh
@@ -14,9 +14,24 @@ aws ecr get-login-password --region us-west-2 | docker login --username AWS --pa
 docker build -t sashetov/gym-data-tracker-app:v1 .
 docker tag sashetov/gym-data-tracker-app:v1 927315517716.dkr.ecr.us-west-2.amazonaws.com/seshsrepo:gym-data-tracker-app-v1
 docker push  927315517716.dkr.ecr.us-west-2.amazonaws.com/seshsrepo:gym-data-tracker-app-v1
+kubectl create secret generic mysql-root-pass --from-literal=password=$MYSQL_ROOT_PASS
+
+kubectl apply -f k8s/mysql-deployment.yaml \
+              -f k8s/mysql-service.yaml
+
+timeout 60 bash -c 'while true; do if kubectl get pods --no-headers | grep "^mysql" | awk '"'"'{print $2}'"'"' | grep -vq "1/1"; then sleep 3; else exit 0; fi; done' || {
+    echo "mysql pod not available, debug why"
+    exit 1;
+} # wait for mysql pod to become available
+MY_POD=$(kubectl get pods --no-headers | grep "^mysql" | awk '{print $1}' | head -n 1)
+kubectl exec -it $MY_POD -- /usr/bin/mysql -u root -p"${MYSQL_ROOT_PASS}" -e "CREATE DATABASE IF NOT EXISTS gymdata;" # make the database
+
+# install the webapp
 kubectl apply -f k8s/webapp-deployment.yaml \
               -f k8s/webapp-service.yaml \
               -f k8s/webapp-service-monitor.yaml \
               -f k8s/webapp-pod-monitor.yaml
-sleep 120 # sleep for DNS
+
+# set up monitoring dashboards
+sleep 120 # wait/sleep for DNS records to be set and propagate
 curl -X POST -H "Content-Type: application/json" -d @grafana/app-containers-dashboard.json "http://admin:${GRAFANA_PASS}@$(kubectl get svc gym-data-tracker-grafana -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}')/api/dashboards/db"
