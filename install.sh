12395d4057a54716b7e89d38470036ff715e970d
Alexander Vassilevski <alexander.vassilevski@gmail.com>
2024-02-20 00:58:56 -0800

    added mysql ebs storage and monitors for mysql

--- install.sh
+++ install.sh
@@ -2,7 +2,18 @@
 . .env
 set -xv
 APP_NS=gym-data-tracker
-eksctl create cluster --name eks-cluster --region us-west-2 --nodegroup-name nodesgroup --node-type t3.medium --nodes 3
+CLUSTER_NAME=eks-cluster
+eksctl create cluster --name $CLUSTER_NAME --region us-west-2 --nodegroup-name nodesgroup --node-type t3.medium --nodes 3
+eksctl utils associate-iam-oidc-provider --cluster $CLUSTER_NAME --approve
+eksctl create iamserviceaccount \
+    --name ebs-csi-controller-sa \
+    --namespace kube-system \
+    --cluster $CLUSTER_NAME \
+    --role-name AmazonEKS_EBS_CSI_DriverRole \
+    --role-only \
+    --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy \
+    --approve
+eksctl create addon --name aws-ebs-csi-driver --cluster $CLUSTER_NAME --service-account-role-arn arn:aws:iam::927315517716:role/AmazonEKS_EBS_CSI_DriverRole --force
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install $APP_NS prometheus-community/kube-prometheus-stack
@@ -16,17 +27,33 @@ docker tag sashetov/gym-data-tracker-app:v1 927315517716.dkr.ecr.us-west-2.amazo
 docker push  927315517716.dkr.ecr.us-west-2.amazonaws.com/seshsrepo:gym-data-tracker-app-v1
 kubectl create secret generic mysql-root-pass --from-literal=password=$MYSQL_ROOT_PASS
 
+#MAKE DB + MONITOR
+kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/ecr/?ref=master"
+until [[ $(kubectl get pods -n kube-system | grep ebs-csi | grep -vc Running) -eq 0 ]]; do echo "Waiting for ebs-csi pods to be running..."; sleep 5; done
+VOLUME_ID=$(aws ec2 create-volume --size 10 --region us-west-2 --availability-zone us-west-2a --volume-type gp2 --query "VolumeId" --output text)
+cp k8s/pv-template.yaml k8s/mysql-pv.yaml
+sed -i "s/YOUR_VOLUME_ID_PLACEHOLDER/$VOLUME_ID/g" k8s/mysql-pv.yaml
+kubectl apply -f k8s/mysql-pv.yaml
+rm -f k8s/mysql-pv.yaml
+kubectl apply -f k8s/mysql-pvc.yaml
+cat <<EOF > k8s/.my.cnf
+[client]
+user=root
+password=$MYSQL_ROOT_PASS
+EOF
+kubectl create configmap mysqld-exporter-config --from-file=k8s/.my.cnf -n default
+rm -f k8s/.my.cnf
 kubectl apply -f k8s/mysql-deployment.yaml \
-              -f k8s/mysql-service.yaml
-
-timeout 60 bash -c 'while true; do if kubectl get pods --no-headers | grep "^mysql" | awk '"'"'{print $2}'"'"' | grep -vq "1/1"; then sleep 3; else exit 0; fi; done' || {
+    -f k8s/mysql-service.yaml \
+    -f k8s/mysql-service-monitor.yaml 
+timeout 60 bash -c 'while true; do if kubectl get pods --no-headers | grep "^mysql" | awk '"'"'{print $2}'"'"' | grep -vq "2/2"; then sleep 3; else exit 0; fi; done' || {
     echo "mysql pod not available, debug why"
     exit 1;
 } # wait for mysql pod to become available
 MY_POD=$(kubectl get pods --no-headers | grep "^mysql" | awk '{print $1}' | head -n 1)
-kubectl exec -it $MY_POD -- /usr/bin/mysql -u root -p"${MYSQL_ROOT_PASS}" -e "CREATE DATABASE IF NOT EXISTS gymdata;" # make the database
+kubectl exec -it $MY_POD -c mysql -- /usr/bin/mysql -u root -p"${MYSQL_ROOT_PASS}" -e "CREATE DATABASE IF NOT EXISTS gymdata;" # make the database
 
-# install the webapp
+# make webapp + monitors
 kubectl apply -f k8s/webapp-deployment.yaml \
               -f k8s/webapp-service.yaml \
               -f k8s/webapp-service-monitor.yaml \
